0b70b461ce9cb44c12360815239baa593908d4fa
Patrick Gremo <patrick.gremo@gmail.com>
2018-02-23 17:10:01 -0600

    improve gatewaycontainer to be more inline with the spec

--- spring-integration-discord/src/main/kotlin/org/springframework/integration/discord/common/DiscordGatewayContainer.kt
+++ spring-integration-discord/src/main/kotlin/org/springframework/integration/discord/common/DiscordGatewayContainer.kt
@@ -18,7 +18,17 @@ import java.util.concurrent.TimeUnit.SECONDS
 import javax.websocket.*
 import javax.websocket.ContainerProvider.getWebSocketContainer
 
-open class DiscordGatewayContainer(private val gatewayUrl: URI, val authorization: String, val scheduler: TaskScheduler, val clock: Clock = Clock.systemUTC()) : SmartLifecycle, Endpoint() {
+@ClientEndpoint(
+        decoders = [(DispatchBinaryDecoder::class), (DispatchTextDecoder::class)],
+        encoders = [(DispatchTextEncoder::class)]
+)
+open class DiscordGatewayContainer(
+        private val gatewayUrl: URI,
+        private val authorization: String,
+        private val scheduler: TaskScheduler,
+        private val clock: Clock = Clock.systemUTC()
+) : SmartLifecycle {
+
     companion object {
         val logger: Log = getLog(DiscordGatewayContainer::class.java)
     }
@@ -29,75 +39,75 @@ open class DiscordGatewayContainer(private val gatewayUrl: URI, val authorizatio
 
     private lateinit var latch: CountDownLatch
 
-    private val config = ClientEndpointConfig.Builder.create()
-            .decoders(mutableListOf(DispatchBinaryDecoder::class.java, DispatchTextDecoder::class.java))
-            .encoders(mutableListOf(DispatchTextEncoder::class.java as Class<out Encoder>))
-            .build()
-
     var session: Session? = null
         get() {
             latch.await(10, SECONDS)
             return field
         }
+        set(value) {
+            field = value
+            latch.countDown()
+        }
 
     var eventHandler: DiscordGatewayMessageProducer? = null
 
-    override fun onOpen(session: Session, config: EndpointConfig) {
-        this.session = RateLimitingSession(session).apply {
-            addMessageHandler(object : MessageHandler.Whole<Dispatch> {
-                private var sequence: Int? = null
-                private var sessionId: String? = null
-                private var heartbeatAck: Boolean = true
-
-                override fun onMessage(dispatch: Dispatch) {
-                    sequence = dispatch.s
-                    val (op, event) = dispatch
-                    when (op) {
-                        Op.Dispatch -> {
-                            when (event) {
-                                is Ready -> sessionId = event.sessionId
-                            }
-                            eventHandler?.onEvent(dispatch)
-                        }
-                        Op.Reconnect -> close()
-                        Op.InvalidSession -> {
-                            if (!(event as Boolean)) {
-                                sequence = null
-                                sessionId = null
-                            }
-                            close()
-                        }
-                        Op.Hello -> {
-                            heartbeat = scheduler.scheduleAtFixedRate({
-                                if (heartbeatAck) {
-                                    heartbeatAck = false
-                                    basicRemote.sendObject(Dispatch(Op.Heartbeat, sequence))
-                                } else {
-                                    close(missingHeartbeatACK)
-                                }
-                            }, Date.from(now(clock)), (event as Hello).heartbeatInterval)
-                            val request = if (sessionId == null) {
-                                Dispatch(Op.Identity, Identity(authorization))
-                            } else {
-                                Dispatch(Op.Resume, Resume(authorization, sessionId!!, sequence))
-                            }
-                            basicRemote.sendObject(request)
-                        }
-                        Op.HeartbeatAck -> heartbeatAck = true
-                        else -> logger.warn("Unrecognized Op code $op")
-                    }
-                }
-            })
-        }
-        latch.countDown()
+    @OnOpen
+    fun onOpen(session: Session) {
+        this.session = RateLimitingSession(session)
     }
 
-    override fun onError(session: Session?, throwable: Throwable?) {
+    private var sequence: Int? = null
+    private var sessionId: String? = null
+    private var heartbeatAck: Boolean = true
+
+    @OnMessage
+    fun apply(dispatch: Dispatch) {
+        sequence = dispatch.s
+        val (op, event) = dispatch
+        when (op) {
+            Op.Dispatch -> {
+                when (event) {
+                    is Ready -> sessionId = event.sessionId
+                }
+                eventHandler?.onEvent(dispatch)
+            }
+            Op.Reconnect -> session!!.close()
+            Op.InvalidSession -> {
+                if (!(event as Boolean)) {
+                    sequence = null
+                    sessionId = null
+                }
+                session!!.close()
+            }
+            Op.Hello -> {
+                heartbeat = scheduler.scheduleAtFixedRate({
+                    if (heartbeatAck) {
+                        heartbeatAck = false
+                        session!!.basicRemote.sendObject(Dispatch(Op.Heartbeat, sequence))
+                    } else {
+                        session!!.close(missingHeartbeatACK)
+                    }
+                }, Date.from(now(clock)), (event as Hello).heartbeatInterval)
+                val request = if (sessionId == null) {
+                    Dispatch(Op.Identity, Identity(authorization))
+                } else {
+                    Dispatch(Op.Resume, Resume(authorization, sessionId!!, sequence))
+                }
+                session!!.basicRemote.sendObject(request)
+            }
+            Op.HeartbeatAck -> heartbeatAck = true
+            else -> logger.warn("Unrecognized Op code $op")
+        }
+    }
+
+    @OnError
+    fun onError(throwable: Throwable?) {
         logger.error("error", throwable)
         if (restarting) scheduler.schedule(this::start, Date.from(now(clock)))
     }
 
-    override fun onClose(session: Session?, closeReason: CloseReason?) {
+    @OnClose
+    fun onClose(closeReason: CloseReason?) {
         logger.info("closing $closeReason")
         heartbeat?.cancel(true)
         restarting = true
@@ -107,7 +117,7 @@ open class DiscordGatewayContainer(private val gatewayUrl: URI, val authorizatio
     override fun start() {
         running = true
         latch = CountDownLatch(1)
-        getWebSocketContainer().connectToServer(this, config, gatewayUrl)
+        getWebSocketContainer().connectToServer(this, gatewayUrl)
     }
 
     override fun isRunning() = running
